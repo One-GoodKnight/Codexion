c82117c3cfb9784fb8020abce826e275570a678a
One-GoodKnight <giniaux.alexandre@gmail.com>
2026-05-18 12:49:59 +0200

    feat: heapify last request of queue

--- src/queue/q_utils.c
+++ src/queue/q_utils.c
@@ -1,24 +1,6 @@
 #include "queue.h"
 #include "utils.h"
 
-void	heapify_last_request(t_queue *queue)
-{
-	t_request	*parent;
-	int	i;
-
-	i = queue->last_i;
-	while (i != 0)
-	{
-		parent = &queue->requests[(i - 1) / 2];
-		if (parent->value > queue->requests[i].value)
-
-	}
-}
-
-void	heapify_first_request(t_queue *queue)
-{
-}
-
 void	swap_requests(t_queue *queue, int one, int two)
 {
 	t_request	temp;
@@ -26,4 +8,25 @@ void	swap_requests(t_queue *queue, int one, int two)
 	ft_memcpy(&temp, &queue->requests[one], sizeof(t_request));
 	ft_memcpy(&queue->requests[one], &queue->requests[two], sizeof(t_request));
 	ft_memcpy(&queue->requests[two], &temp, sizeof(t_request));
+}
+
+void	heapify_last_request(t_queue *queue)
+{
+	int	i;
+	int	i_parent;
+
+	i = queue->last_i;
+	while (i != 0)
+	{
+		i_parent = (i - 1) / 2;
+		if (queue->requests[i_parent].value <= queue->requests[i].value)
+			return ;
+		swap_requests(queue, i_parent, i);
+		i = i_parent;
+	}
+}
+
+void	heapify_first_request(t_queue *queue)
+{
+
 }
