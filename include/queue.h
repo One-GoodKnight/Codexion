9436b95632175c7e618eec065dbdc75a0f901c66
One-GoodKnight <giniaux.alexandre@gmail.com>
2026-05-18 11:34:33 +0200

    refactor: queue make coders array of references

--- include/queue.h
+++ include/queue.h
@@ -5,11 +5,12 @@
 
 typedef struct	s_queue
 {
-	t_coder	*coders;
+	t_coder	**coders;
 	int		last_i;
 	int		max_i;
 }	t_queue;
 
+int		q_init(t_queue *queue, int size);
 void	q_insert(t_queue *queue, t_coder *coder);
 void	q_extract(t_queue *queue);
 void	q_decrease(t_coder *coder);
--- src/queue.c
+++ src/queue.c
@@ -1,12 +1,17 @@
 #include "queue.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void	q_init(t_queue *queue, int size)
+int	q_init(t_queue *queue, int size)
 {
+	queue->coders = malloc(sizeof(t_coder *) * size);
+	if (!queue->coders)
+		return (-1);
 	memset(queue, 0, sizeof(t_queue));
 	queue->last_i = -1;
 	queue->max_i = size - 1;
+	return (0);
 }
 
 void	q_insert(t_queue *queue, t_coder *coder)
@@ -17,12 +22,12 @@ void	q_insert(t_queue *queue, t_coder *coder)
 		return ;
 	}
 	queue->last_i++;
-	queue->coders[queue->last_i] = *coder;
+	queue->coders[queue->last_i] = coder;
 }
 
 void	q_extract(t_queue *queue)
 {
-
+	
 }
 
 void	q_decrease(t_node *node)
